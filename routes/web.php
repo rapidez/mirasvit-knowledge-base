74c28d9fde0210c3cea107ceb3b02c2750718e0f
Roy Duineveld <royduineveld@gmail.com>
2021-11-04 13:49:34 +0100

    Web middleware on routes

--- routes/web.php
+++ routes/web.php
@@ -6,37 +6,39 @@ use Rapidez\MirasvitKnowledgeBase\Models\Article;
 use Rapidez\MirasvitKnowledgeBase\Models\Category;
 use Rapidez\MirasvitKnowledgeBase\Models\Rewrite;
 
-$prefix = Rapidez::config('kb/general/base_url', 'knowledge-base');
+Route::middleware('web')->group(function () {
+    $prefix = Rapidez::config('kb/general/base_url', 'knowledge-base');
 
-Route::get($prefix.'/{slug}', function ($slug) {
-    $rewrite = Rewrite::where('url_key', $slug)->firstOrFail();
+    Route::get($prefix.'/{slug}', function ($slug) {
+        $rewrite = Rewrite::where('url_key', $slug)->firstOrFail();
 
-    if ($rewrite->type == 'CATEGORY') {
-        $category = Category::findOrFail($rewrite->entity_id);
-        $categories = Category::with('subCategories')
+        if ($rewrite->type == 'CATEGORY') {
+            $category = Category::findOrFail($rewrite->entity_id);
+            $categories = Category::with('subCategories')
+                ->orderBy('position')
+                ->where('parent_id', $category->category_id)
+                ->get();
+            $articles = $category->articles()->paginate(10);
+
+            return view('mirasvitknowledgebase::category', compact('category', 'categories', 'articles'));
+        }
+
+        if ($rewrite->type == 'ARTICLE') {
+            $article = Article::findOrFail($rewrite->entity_id);
+
+            return view('mirasvitknowledgebase::article', compact('article'));
+        }
+
+        abort(404);
+    })->name('knowledgebase')->where('slug', '.*');
+
+    Route::get($prefix, function () {
+        $category = Category::getRootCategory();
+        $categories = Category::with(['subCategories', 'articles'])
             ->orderBy('position')
             ->where('parent_id', $category->category_id)
             ->get();
-        $articles = $category->articles()->paginate(10);
 
-        return view('mirasvitknowledgebase::category', compact('category', 'categories', 'articles'));
-    }
-
-    if ($rewrite->type == 'ARTICLE') {
-        $article = Article::findOrFail($rewrite->entity_id);
-
-        return view('mirasvitknowledgebase::article', compact('article'));
-    }
-
-    abort(404);
-})->name('knowledgebase')->where('slug', '.*');
-
-Route::get($prefix, function () {
-    $category = Category::getRootCategory();
-    $categories = Category::with(['subCategories', 'articles'])
-        ->orderBy('position')
-        ->where('parent_id', $category->category_id)
-        ->get();
-
-    return view('mirasvitknowledgebase::category', compact('category', 'categories'));
+        return view('mirasvitknowledgebase::category', compact('category', 'categories'));
+    });
 });
